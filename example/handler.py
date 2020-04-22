89750bf46cbb278cd78b1860ccf58febfad412a4
AndreeaPastinaru <32838633+AndreeaPastinaru@users.noreply.github.com>
2020-04-22 09:29:11 +0200

    Update handler.py

--- example/handler.py
+++ example/handler.py
@@ -1,14 +1,9 @@
-import time
 
 def handle(data, client):
-    print("Hello anders. Will sleep a bit now...")
-    time.sleep(1.0)
-    print("Did sleep. Will sleep some more, but first, print another line")
-    print("Here it is.")
-    time.sleep(1.0)
-    print("Ok, I'm done now. Fetching an asset")
-    asset = client.assets.retrieve(external_id = "my_special_asset")
+    print("Hello")
+    
+    asset = client.assets.retrieve(id=370758465950)
     return {
-        "result": 2.0 * data["value"],
-        "assetName": asset.name,
+        "result": asset.id,
+        "assetName": asset.desciption,
     }
